f4dbae7f1262b112c7e07a629cc2ec494bee09b8
Maurits van der Schee <maurits@vdschee.nl>
2014-06-01 01:25:03 +0200

    Performance improvement

--- pages/admin/posts/index().php
+++ pages/admin/posts/index().php
@@ -1,2 +1,2 @@
 <?php
-$rows = DB::q('select * from posts,users where user_id = users.id');
+$rows = DB::q('select posts.title,posts.tags,posts.published,users.id,users.username from posts,users where user_id = users.id');

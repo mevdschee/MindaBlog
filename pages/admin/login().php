9858f19e19032c208a25d3480fff66a39fc77aa4
Maurits van der Schee <maurits@vdschee.nl>
2014-05-31 02:40:49 +0200

    Avoid code duplication include file

--- pages/admin/login().php
+++ pages/admin/login().php
@@ -2,12 +2,7 @@
 $error = '';
 if (isset($_POST['username']))
 { if (Auth::login($_POST['username'],$_POST['password'])) {
-	$rows = DB::q('select * from settings');
-	$settings = array();
-	array_walk($rows, function($v,$k) use (&$settings) { 
-		$settings[$v['settings']['key']] = $v['settings']['value']; 
-	});
-	$_SESSION['settings'] = $settings;
+	include __DIR__.'/settings/index().php';
     Router::redirect("admin");
   }
   else $error = "Username/password not valid";
--- pages/admin/settings/index().php
+++ pages/admin/settings/index().php
@@ -1,2 +1,7 @@
 <?php
 $rows = DB::q('select * from settings');
+$settings = array();
+array_walk($rows, function($v,$k) use (&$settings) { 
+	$settings[$v['settings']['key']] = $v['settings']['value']; 
+});
+$_SESSION['settings'] = $settings;

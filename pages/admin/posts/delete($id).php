4f7af4a8d6791b439f8436ecef8f943bddf51f16
Maurits van der Schee <maurits@vdschee.nl>
2014-08-21 01:51:01 +0200

    Add delete pages

---
+++ pages/admin/posts/delete($id).php
@@ -0,0 +1,7 @@
+<?php
+if (!empty($_POST)) {
+	$rows = Query::delete('DELETE FROM posts WHERE id = ?', $id);
+	if ($rows) $_SESSION['flash_messages'][] = 'success: Post deleted';
+	else $_SESSION['flash_messages'][] = 'danger: Post not deleted';
+	Router::redirect('admin/posts/index');
+}

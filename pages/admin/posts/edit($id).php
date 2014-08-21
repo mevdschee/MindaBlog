fc1ac9e7f6ee2b862422a613aa39954fee439e93
Maurits van der Schee <maurits@vdschee.nl>
2014-08-22 00:21:28 +0200

    Add slug

--- pages/admin/posts/edit($id).php
+++ pages/admin/posts/edit($id).php
@@ -1,6 +1,7 @@
 <?php
 if (!empty($_POST)) {
-	$rows = DB::update('UPDATE posts SET title = ?, content = ? WHERE id = ?',$_POST['title'], $_POST['content'], $id);
+	$slug = preg_replace('/--+/','-',trim(preg_replace('/[^\-a-z0-9]/','-',strtolower($_POST['title'])),'-'));
+	$rows = DB::update('UPDATE posts SET slug = ?, title = ?, content = ? WHERE id = ?', $slug, $_POST['title'], $_POST['content'], $id);
 	if ($rows) $_SESSION['flash_messages'][] = 'success: Post updated';
 	else $_SESSION['flash_messages'][] = 'danger: Post not updated';
 	Router::redirect('admin/posts/index');

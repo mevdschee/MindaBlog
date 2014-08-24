2bbc3a4601c72d7853457596e35d4b8eab85ae6d
Maurits van der Schee <maurits@vdschee.nl>
2014-08-24 23:04:05 +0200

    Update

--- pages/admin/posts/edit($id).php
+++ pages/admin/posts/edit($id).php
@@ -1,6 +1,7 @@
 <?php
 if (!empty($_POST)) {
 	if (!preg_match('/^[a-z0-9-]+$/', $_POST['posts']['slug'])) $errors['posts[slug]']='Only letters, numbers and the minus character are allowed';
+	if (DB::selectValue('SELECT slug FROM posts where slug = ? and id != ?', $_POST['posts']['slug'], $id)) $errors['posts[slug]']='Already taken';
 	if (!isset($errors)) {
 		$rowsAffected = DB::update('UPDATE posts SET slug = ?, title = ?, content = ? WHERE id = ?', $_POST['posts']['slug'], $_POST['posts']['title'], $_POST['posts']['content'], $id);
 		if ($rowsAffected!==false) {
@@ -8,7 +9,7 @@ if (!empty($_POST)) {
 			Router::redirect('admin/posts/index');
 		}
 	}
-	$flash['danger'] = 'Post not updated'; 
+	$flash['danger'] = 'Post not saved'; 
 } else {
 	$_POST = DB::selectOne('SELECT * from posts where id = ?', $id);
 }

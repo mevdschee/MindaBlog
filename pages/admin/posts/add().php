425430ce735fc5e7e4f6f841551630a6c3b82951
Maurits van der Schee <maurits@vdschee.nl>
2014-08-24 21:22:14 +0200

    new db layout

--- pages/admin/posts/add().php
+++ pages/admin/posts/add().php
@@ -1,7 +1,7 @@
 <?php
 if (!empty($_POST)) {
 	$_POST['posts']['slug'] = date('Y-m-').preg_replace('/--+/','-',trim(preg_replace('/[^\-a-z0-9]/','-',strtolower($_POST['posts']['title'])),'-'));
-	$id = DB::insert('INSERT INTO posts (slug,title,content,user_id) VALUES (?, ?, ?, ?)',$_POST['posts']['slug'],$_POST['posts']['title'], $_POST['posts']['content'], $_SESSION['user']['id']);
+	$id = DB::insert('INSERT INTO posts (slug,title,content,user_id) VALUES (?, ?, ?, ?)', $_POST['posts']['slug'], $_POST['posts']['title'], $_POST['posts']['content'], $_SESSION['user']['id']);
 	if ($id) {
 		$_SESSION['flash']['success'] = 'Post saved';
 		Router::redirect('admin/posts/index');

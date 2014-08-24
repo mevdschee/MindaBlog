3c5c3d8f489cb6b7be14bd3ad8edb6b285b2f93e
Maurits van der Schee <maurits@vdschee.nl>
2014-08-25 00:46:30 +0200

    Automatic slug creation

--- pages/admin/posts/add().php
+++ pages/admin/posts/add().php
@@ -1,10 +1,14 @@
 <?php
 if (!empty($_POST)) {
-	$_POST['posts']['slug'] = date('Y-m-').preg_replace('/--+/','-',trim(preg_replace('/[^\-a-z0-9]/','-',strtolower($_POST['posts']['title'])),'-'));
-	$id = DB::insert('INSERT INTO posts (slug,title,content,user_id) VALUES (?, ?, ?, ?)', $_POST['posts']['slug'], $_POST['posts']['title'], $_POST['posts']['content'], $_SESSION['user']['id']);
-	if ($id) {
-		$_SESSION['flash']['success'] = 'Post saved';
-		Router::redirect('admin/posts/index');
+	if (!isset($_POST['posts']['slug'])) $_POST['posts']['slug'] = date('Y-m-').preg_replace('/--+/','-',trim(preg_replace('/[^\-a-z0-9]/','-',strtolower($_POST['posts']['title'])),'-'));
+	if (!preg_match('/^[a-z0-9-]+$/', $_POST['posts']['slug'])) $errors['posts[slug]']='Only letters, numbers and the minus character are allowed';
+	if (DB::selectValue('SELECT slug FROM posts where slug = ?', $_POST['posts']['slug'])) $errors['posts[slug]']='Already taken';
+	if (!isset($errors)) {
+		$id = DB::insert('INSERT INTO posts (slug,title,content,user_id) VALUES (?, ?, ?, ?)', $_POST['posts']['slug'], $_POST['posts']['title'], $_POST['posts']['content'], $_SESSION['user']['id']);
+		if ($id) {
+			$_SESSION['flash']['success'] = 'Post saved';
+			Router::redirect('admin/posts/index');
+		}
 	}
 	$flash['danger'] = 'Post not saved';	
 } else {

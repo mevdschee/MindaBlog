b1ed6a011dd8fe1a91b7f035d7ae34b6fac4e00a
Maurits van der Schee <maurits@vdschee.nl>
2014-08-24 03:08:49 +0200

    Flash messages improved

--- templates/admin.php
+++ templates/admin.php
@@ -9,9 +9,9 @@ array_walk($menu, function(&$item,$url) {
 	$item['active'] = substr(Router::getUrl(),0,strlen($url))==$url?'active':''; 
 });
 
-if (isset($_SESSION['flash_messages'])) {
-	$flashMessages = $_SESSION['flash_messages'];
-	unset($_SESSION['flash_messages']);
+if (isset($_SESSION['flash'])) {
+	$flash = $_SESSION['flash'];
+	unset($_SESSION['flash']);
 } else {
-	$flashMessages = array();
+	$flash = array();
 }

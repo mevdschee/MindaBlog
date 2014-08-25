308207c19e33a1b689c1ec6b50615c7678628cda
Maurits van der Schee <maurits@vdschee.nl>
2014-08-25 02:49:17 +0200

    Prettyfied it

--- pages/admin/posts/index().php
+++ pages/admin/posts/index().php
@@ -1,2 +1,2 @@
 <?php
-$rows = DB::select('select posts.id,posts.title,posts.tags,posts.published,users.id,users.username from posts,users where user_id = users.id');
+$rows = DB::select('select posts.id,posts.slug,posts.title,posts.tags,posts.published,users.id,users.username from posts,users where user_id = users.id');

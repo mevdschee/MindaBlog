89e164011c38ea24b8fd0fe0a35b76a333249ed6
Maurits van der Schee <maurits@vdschee.nl>
2016-02-19 23:06:53 +0100

    Improved layout

--- pages/index($slug).php
+++ pages/index($slug).php
@@ -1,6 +1,8 @@
 <?php
 $data = DB::selectOne('select * from posts where slug = ?',$slug);
 if (!$data) Router::redirect('error/not_found');
+DB::delete('delete from `daily_views` where `created` < DATE_SUB(NOW(), INTERVAL 7 DAY)');
+DB::insert('insert into `daily_views` (`posts_id`,`created`) values (?,NOW())',$data['posts']['id']);
 
 if (preg_match('/\r?\n---+\r?\n/', $data['posts']['content'])) {
 	list($before,$after) = preg_split('/\r?\n---+\r?\n/', $data['posts']['content'], 2);
--- templates/default.php
+++ templates/default.php
@@ -3,3 +3,15 @@
 if (!isset($_SESSION['settings'])) {
     $_SESSION['settings'] = DB::selectPairs('select `key`,`value` from settings');
 }
+
+//$popular = Cache::get("popular");
+//if (!$popular) {
+    $popular = DB::selectPairs('select `slug`,`title`,count(`posts_id`) as `views` from `posts`,`daily_views` where `posts`.`id`=`daily_views`.`posts_id` group by `posts_id` order by `views` desc');
+//    Cache::set("popular",$popular,300);
+//}
+
+//$latest = Cache::get("latest");
+//if (!$latest) {
+    $latest = DB::selectPairs('select `slug`,`title` from `posts` order by `published` DESC LIMIT 10');
+//    Cache::set("latest",$latest,300);
+//}

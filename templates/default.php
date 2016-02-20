c3450b6808e43fcc637058151b7d7d18fab4c19f
Maurits van der Schee <maurits@vdschee.nl>
2016-02-20 20:11:37 +0100

    bugfix

--- templates/default.php
+++ templates/default.php
@@ -13,7 +13,7 @@ if (!$stats) {
     $stats = array();
     $stats['popular_posts'] = DB::selectPairs('select `slug`,`title`,count(`posts_id`) as `views` from `posts`,`unique_views` where `posts`.`id`=`unique_views`.`posts_id` group by `posts_id` order by `views` desc limit 10');
     $stats['latest_posts'] = DB::selectPairs('select `slug`,`title` from `posts` order by `published` desc limit 10');
-    $stats['visitors_day'] = DB::selectValue('select count(id) from `unique_visitors` where `ip`=? and `day` = ?',$_SERVER['REMOTE_ADDR'],date('Y-m-d'));
-    $stats['visitors_month'] = DB::selectValue('select count(id) from `unique_visitors` where `ip`=? and `day` >= ?',$_SERVER['REMOTE_ADDR'],date('Y-m-01'));
+    $stats['visitors_day'] = DB::selectValue('select count(id) from `unique_visitors` where `day` = ?',date('Y-m-d'));
+    $stats['visitors_month'] = DB::selectValue('select count(id) from `unique_visitors` where `day` >= ?',date('Y-m-01'));
     Cache::set("stats",$stats,300);
 }

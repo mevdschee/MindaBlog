bfa43fea0663f648b51ceea39498abdb6e4c7f97
Maurits van der Schee <maurits@vdschee.nl>
2016-03-01 00:08:40 +0100

    bugfix

--- pages/index($slug).php
+++ pages/index($slug).php
@@ -1,7 +1,7 @@
 <?php
 $data = DB::selectOne('select * from posts where slug = ?',$slug);
 if (!$data) Router::redirect('error/not_found');
-DB::insert('insert into `unique_views` (`posts_id`,`ip`,`day`,`requests`) values (?,?,DATE(NOW()),1) ON DUPLICATE KEY UPDATE `requests`=`requests`+1;',$data['posts']['id'],$_SERVER['REMOTE_ADDR']);
+DB::insert('insert into `unique_views` (`post_id`,`ip`,`day`,`requests`) values (?,?,DATE(NOW()),1) ON DUPLICATE KEY UPDATE `requests`=`requests`+1;',$data['posts']['id'],$_SERVER['REMOTE_ADDR']);
 if (preg_match('/\(?\.\.\.+\)?/', $data['posts']['content'])) {
 	$data['posts']['content'] = preg_replace('/\(?\.\.\.+\)?/', '', $data['posts']['content'], 1);
 }

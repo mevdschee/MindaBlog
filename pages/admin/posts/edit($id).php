4d126eacef8efd05a0fb7252f77040966f5fc553
Maurits van der Schee <maurits@vdschee.nl>
2016-03-01 00:38:05 +0100

    bugfix

--- pages/admin/posts/edit($id).php
+++ pages/admin/posts/edit($id).php
@@ -4,12 +4,12 @@ if (!empty($_POST)) {
 	if (!$_POST['posts']['published']) $_POST['posts']['published'] = null;
 	if (DB::selectValue('SELECT slug FROM posts where slug = ? and id != ?', $_POST['posts']['slug'], $id)) $errors['posts[slug]']='Already taken';
 	if (!isset($errors)) {
-		$rowsAffected = DB::update('UPDATE posts SET published = ?, slug = ?, title = ?, content = ?, word_count = ? WHERE id = ?', $_POST['posts']['published'], $_POST['posts']['slug'], $_POST['posts']['title'], $_POST['posts']['content'], str_word_count($_POST['posts']['content']), $id);
+		$rowsAffected = DB::update('UPDATE posts SET published = ?, slug = ?, title = ?, content = ?, word_count = ?, modified = NOW() WHERE id = ?', $_POST['posts']['published'], $_POST['posts']['slug'], $_POST['posts']['title'], $_POST['posts']['content'], str_word_count($_POST['posts']['content']), $id);
 		if ($rowsAffected!==false) {
 			Router::redirect('admin/posts/index');
 		}
 	}
-	$flash['danger'] = 'Post not saved'; 
+	$flash['danger'] = 'Post not saved';
 } else {
 	$_POST = DB::selectOne('SELECT * from posts where id = ?', $id);
 }

69ece573e3e2fe116e115b1298f4ae0d6aa30ed4
Maurits van der Schee <maurits@vdschee.nl>
2016-03-01 00:07:50 +0100

    bugfix

--- pages/admin/posts/index().php
+++ pages/admin/posts/index().php
@@ -1,8 +1,9 @@
 <?php
-$data = DB::select('select posts.id,posts.slug,posts.title,posts.word_count,posts.tags,posts.published,users.id,users.username,count(unique_views.ip) as `unique_views.visitors` from posts,users,unique_views where posts.user_id = users.id and unique_views.post_id = posts.id group by unique_views.post_id order by published is not null,published desc');
+$data = DB::select('select posts.id,posts.slug,posts.title,posts.word_count,posts.tags,posts.published,users.id,users.username from posts,users where posts.user_id = users.id order by published is not null,published desc');
 foreach (array_keys($data) as $i) {
 	$data[$i]['posts']['published_label_type'] = '';
 	$data[$i]['posts']['published_label_text'] = '';
+	$data[$i]['unique_views']['visitors'] = DB::selectValue('select count(unique_views.ip) as `unique_views.visitors` from unique_views where unique_views.post_id = ? group by unique_views.post_id;',$data[$i]['posts']['id']);
 	if ($data[$i]['posts']['published']) {
 		list($data[$i]['posts']['published_day'],$data[$i]['posts']['published_month']) = explode('-',date('d-M',strtotime($data[$i]['posts']['published'])));
 		if (strcmp($data[$i]['posts']['published'],date('Y-m-d'))>0) {

d8f87513a02818f5ab7691fb9c1ea389f6e62983
Maurits van der Schee <maurits@vdschee.nl>
2016-03-01 02:00:38 +0100

    bugfix

--- pages/index($slug).php
+++ pages/index($slug).php
@@ -2,8 +2,8 @@
 $data = DB::selectOne('select * from posts where slug = ? and published is not null and published < NOW();',$slug);
 if (!$data) Router::redirect('error/not_found');
 DB::insert('insert into `unique_views` (`post_id`,`ip`,`day`,`requests`) values (?,?,DATE(NOW()),1) ON DUPLICATE KEY UPDATE `requests`=`requests`+1;',$data['posts']['id'],$_SERVER['REMOTE_ADDR']);
-if (preg_match('/\(?\.\.\.+\)?/', $data['posts']['content'])) {
-	$data['posts']['content'] = preg_replace('/\(?\.\.\.+\)?/', '', $data['posts']['content'], 1);
+if ($pos=strpos($data['posts']['content'],'(more)')) {
+	$data['posts']['content'] = substr_replace($data['posts']['content'], '', $pos, 6);
 }
 $title = $data['posts']['title'];
 Buffer::set('content',Michelf\Markdown::defaultTransform($data['posts']['content']));

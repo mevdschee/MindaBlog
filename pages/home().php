32868c877316d0ee12e472325d51a8fbb66a7c33
Maurits van der Schee <maurits@vdschee.nl>
2016-03-01 02:10:01 +0100

    bugfix

--- pages/home().php
+++ pages/home().php
@@ -1,8 +1,8 @@
 <?php
 $data = DB::select('select * from posts where published is not null and published < NOW() order by published desc limit 5');
 foreach (array_keys($data) as $i) {
-	if (preg_match('/\(?\.\.\.+\)/', $data[$i]['posts']['content'], $matches, PREG_OFFSET_CAPTURE)) {
-		$data[$i]['posts']['html'] = substr($data[$i]['posts']['content'], 0, $matches[0][1]).'...';
+	if ($pos = strpos($data[$i]['posts']['content'], '(...)')) {
+		$data[$i]['posts']['html'] = substr_replace($data[$i]['posts']['content'], '', $pos, 5).'...';
         $data[$i]['posts']['more_html'] = true;
 	} else {
 		$data[$i]['posts']['html'] = $data[$i]['posts']['content'];

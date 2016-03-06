5319568b61831d5977a3cfdb1a23ef7bf1e53b89
Maurits van der Schee <maurits@vdschee.nl>
2016-03-06 11:16:24 +0100

    Add visitor overview

--- pages/admin/index().php
+++ pages/admin/index().php
@@ -3,9 +3,6 @@
 $stats = Cache::get("admin_stats");
 if (!$stats) {
     $stats = array();
-    $stats['popular_posts'] = DB::selectPairs('select `slug`,`title`,count(`post_id`) as `views` from `posts`,`unique_views` where `posts`.`id`=`unique_views`.`post_id` and `posts`.`published` is not null and `posts`.`published` < NOW() group by `post_id` order by `views` desc limit 10');
-    $stats['latest_posts'] = DB::selectPairs('select `slug`,`title` from `posts` where `posts`.`published` is not null and `posts`.`published` < NOW() order by `published` desc limit 10');
-    $stats['visitors_day'] = DB::selectValue('select count(id) from `unique_visitors` where `day` = ?',date('Y-m-d'));
-    $stats['visitors_month'] = DB::selectValue('select count(id) from `unique_visitors` where `day` >= ?',date('Y-m-01'));
+    $stats['visitors'] = DB::select('select `day`,count(id) as "unique_visitors.visitors",sum(`requests`) as "unique_visitors.views" from `unique_visitors` where `day` >= NOW() - INTERVAL 30 DAY group by `day`');
     Cache::set("admin_stats",$stats,30);
 }

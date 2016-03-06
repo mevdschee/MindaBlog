798061fefc019ef4a3c7340178e245e8e6991828
Maurits van der Schee <maurits@vdschee.nl>
2016-03-06 10:14:22 +0100

    Add 5 min stats

--- pages/admin/index().php
+++ pages/admin/index().php
@@ -1,1 +1,11 @@
-<?php 
+<?php
+
+$stats = Cache::get("admin_stats");
+if (!$stats) {
+    $stats = array();
+    $stats['popular_posts'] = DB::selectPairs('select `slug`,`title`,count(`post_id`) as `views` from `posts`,`unique_views` where `posts`.`id`=`unique_views`.`post_id` and `posts`.`published` is not null and `posts`.`published` < NOW() group by `post_id` order by `views` desc limit 10');
+    $stats['latest_posts'] = DB::selectPairs('select `slug`,`title` from `posts` where `posts`.`published` is not null and `posts`.`published` < NOW() order by `published` desc limit 10');
+    $stats['visitors_day'] = DB::selectValue('select count(id) from `unique_visitors` where `day` = ?',date('Y-m-d'));
+    $stats['visitors_month'] = DB::selectValue('select count(id) from `unique_visitors` where `day` >= ?',date('Y-m-01'));
+    Cache::set("admin_stats",$stats,30);
+}
--- templates/default.php
+++ templates/default.php
@@ -4,14 +4,15 @@ if (!isset($_SESSION['settings'])) {
     $_SESSION['settings'] = DB::selectPairs('select `key`,`value` from settings');
 }
 
-DB::insert('insert into `unique_visitors` (`ip`,`day`,`requests`) values (?,DATE(NOW()),1) ON DUPLICATE KEY UPDATE `requests`=`requests`+1;',$_SERVER['REMOTE_ADDR']);
+DB::insert('insert into `unique_visitors` (`ip`,`day`,`requests`,`last_seen`) values (?,DATE(NOW()),1,NOW()) ON DUPLICATE KEY UPDATE `requests`=`requests`+1, `last_seen`=NOW();',$_SERVER['REMOTE_ADDR']);
 
 $stats = Cache::get("stats");
 if (!$stats) {
     $stats = array();
     $stats['popular_posts'] = DB::selectPairs('select `slug`,`title`,count(`post_id`) as `views` from `posts`,`unique_views` where `posts`.`id`=`unique_views`.`post_id` and `posts`.`published` is not null and `posts`.`published` < NOW() group by `post_id` order by `views` desc limit 10');
     $stats['latest_posts'] = DB::selectPairs('select `slug`,`title` from `posts` where `posts`.`published` is not null and `posts`.`published` < NOW() order by `published` desc limit 10');
-    $stats['visitors_day'] = DB::selectValue('select count(id) from `unique_visitors` where `day` = ?',date('Y-m-d'));
+    $stats['visitors_5_min'] = DB::selectValue('select count(id) from `unique_visitors` where `last_seen` > NOW() - INTERVAL 5 MINUTE');
+    $stats['visitors_today'] = DB::selectValue('select count(id) from `unique_visitors` where `day` = ?',date('Y-m-d'));
     $stats['visitors_month'] = DB::selectValue('select count(id) from `unique_visitors` where `day` >= ?',date('Y-m-01'));
-    Cache::set("stats",$stats,300);
+    Cache::set("stats",$stats,30);
 }

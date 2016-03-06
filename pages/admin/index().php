3f109320fab5227b181d44249bb9b87bc258a48c
Maurits van der Schee <maurits@vdschee.nl>
2016-03-06 11:44:43 +0100

    Add visitor overview

--- pages/admin/index().php
+++ pages/admin/index().php
@@ -4,5 +4,6 @@ $stats = Cache::get("admin_stats");
 if (!$stats) {
     $stats = array();
     $stats['visitors'] = DB::select('select `day`,count(id) as "unique_visitors.visitors",sum(`requests`) as "unique_visitors.views" from `unique_visitors` where `day` >= NOW() - INTERVAL 30 DAY group by `day`');
+    $stats['posts'] = DB::selectPairs('select DATE(`published`) as "posts.published_date",`slug` from `posts` where `published` >= NOW() - INTERVAL 30 DAY');
     Cache::set("admin_stats",$stats,30);
 }

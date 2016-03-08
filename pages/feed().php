50243de0ece2896bf46bd7278a38d2c738918d82
Maurits van der Schee <maurits@vdschee.nl>
2016-03-08 01:14:05 +0100

    bugfix

--- pages/feed().php
+++ pages/feed().php
@@ -2,7 +2,7 @@
 if (!isset($_SESSION['settings'])) {
     $_SESSION['settings'] = DB::selectPairs('select `key`,`value` from settings');
 }
-$data = DB::select('select * from posts,users where posts.user_id = users.id and published is not null and published < NOW() order by published desc limit 10',$limit);
+$data = DB::select('select * from posts,users where posts.user_id = users.id and published is not null and published < NOW() order by published desc limit 10');
 foreach (array_keys($data) as $i) {
   if ($pos=strpos($data[$i]['posts']['content'],'(...)')) {
   	$data[$i]['posts']['content'] = substr_replace($data[$i]['posts']['content'], '', $pos, 5);
--- pages/home().php
+++ pages/home().php
@@ -1,6 +1,5 @@
 <?php
-if (!isset($limit)) $limit = 5;
-$data = DB::select('select * from posts,users where posts.user_id = users.id and published is not null and published < NOW() order by published desc limit ?',$limit);
+$data = DB::select('select * from posts,users where posts.user_id = users.id and published is not null and published < NOW() order by published desc limit 5');
 foreach (array_keys($data) as $i) {
 	if ($pos = strpos($data[$i]['posts']['content'], '(...)')) {
 		$data[$i]['posts']['html'] = substr($data[$i]['posts']['content'], 0, $pos).'...';

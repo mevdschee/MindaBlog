46191e3aa52a16b699a7c312324359ebfac2d410
Maurits van der Schee <maurits@vdschee.nl>
2016-03-08 22:02:23 +0100

    bugfix

--- pages/index($slug).php
+++ pages/index($slug).php
@@ -1,5 +1,5 @@
 <?php
-$data = DB::selectOne('select * from posts,users where posts.user_id = users.id and slug = ? and published is not null and published < NOW() order by published desc;',$slug);
+$data = DB::selectOne('select * from posts,users where posts.user_id = users.id and slug = ? and published is not null and published < NOW();',$slug);
 if (!$data) Router::redirect('error/not_found');
 DB::insert('insert into `unique_views` (`post_id`,`ip`,`day`,`requests`) values (?,?,DATE(NOW()),1) ON DUPLICATE KEY UPDATE `requests`=`requests`+1;',$data['posts']['id'],$_SERVER['REMOTE_ADDR']);
 if ($pos=strpos($data['posts']['content'],'(...)')) {

f0d7369acb6c762db1400ac1b0755dc4aeea7a4c
Maurits van der Schee <maurits@vdschee.nl>
2016-06-03 12:21:06 +0200

    meta description

--- pages/home().php
+++ pages/home().php
@@ -4,9 +4,12 @@ foreach (array_keys($data) as $i) {
 	if ($pos = strpos($data[$i]['posts']['content'], '(...)')) {
 		$data[$i]['posts']['html'] = substr($data[$i]['posts']['content'], 0, $pos).'...';
 		$data[$i]['posts']['more_html'] = true;
+	} else if ($pos = strpos($data['posts']['content'],array('.',' ',','),200)) {
+		$data[$i]['posts']['html'] = substr($data[$i]['posts']['content'], 0, $pos).'...';
+		$data[$i]['posts']['more_html'] = true;
 	} else {
 		$data[$i]['posts']['html'] = $data[$i]['posts']['content'];
-    $data[$i]['posts']['more_html'] = false;
+		$data[$i]['posts']['more_html'] = false;
 	}
 	Buffer::set("content[$i]",trim(Michelf\Markdown::defaultTransform($data[$i]['posts']['html'])));
 }
--- pages/index($slug).php
+++ pages/index($slug).php
@@ -4,6 +4,11 @@ if (!$data) Router::redirect('error/not_found');
 DB::insert('insert into `unique_views` (`post_id`,`ip`,`day`,`requests`) values (?,?,DATE(NOW()),1) ON DUPLICATE KEY UPDATE `requests`=`requests`+1;',$data['posts']['id'],$_SERVER['REMOTE_ADDR']);
 if ($pos=strpos($data['posts']['content'],'(...)')) {
 	$data['posts']['content'] = substr_replace($data['posts']['content'], '', $pos, 5);
+	$data['posts']['description'] = substr($data['posts']['content'],0,$pos).'...';
+} else if ($pos = strpos($data['posts']['content'],array('.',' ',','),200)) {
+	$data['posts']['description'] = substr($data['posts']['content'],0,$pos).'...';
+} else {
+	$data['posts']['description'] = $data['posts']['content'];
 }
 $title = $data['posts']['title'];
 Buffer::set('content',Michelf\Markdown::defaultTransform($data['posts']['content']));

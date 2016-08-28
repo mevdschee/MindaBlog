4503d3fdab500d9f2a3409c87831cd9dd5d29ba0
Maurits van der Schee <maurits@vdschee.nl>
2016-08-28 10:51:55 +0200

    log other pages

--- pages/home().php
+++ pages/home().php
@@ -1,5 +1,6 @@
 <?php
 $data = DB::select('select * from posts,users where posts.user_id = users.id and published is not null and published < NOW() order by published desc limit 10');
+DB::insert('insert into `unique_views` (`type`,`post_id`,`ip`,`day`,`requests`) values (1,NULL,?,DATE(NOW()),1) ON DUPLICATE KEY UPDATE `requests`=`requests`+1;',$_SERVER['REMOTE_ADDR']);
 foreach (array_keys($data) as $i) {
 	if ($pos = strpos($data[$i]['posts']['content'], '(...)')) {
 		$data[$i]['posts']['html'] = substr($data[$i]['posts']['content'], 0, $pos).'...';
--- pages/sitemap.xml().php
+++ pages/sitemap.xml().php
@@ -1,2 +1,3 @@
 <?php
 $data = DB::select('select slug, modified from posts where published is not null and published < NOW() order by published');
+DB::insert('insert into `unique_views` (`type`,`post_id`,`ip`,`day`,`requests`) values (4,NULL,?,DATE(NOW()),1) ON DUPLICATE KEY UPDATE `requests`=`requests`+1;',$_SERVER['REMOTE_ADDR']);

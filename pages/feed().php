8f0541d4b80733ad5f5d2666eb8665790dbb1659
Maurits van der Schee <maurits@vdschee.nl>
2016-08-28 11:13:44 +0200

    make types string

--- pages/feed().php
+++ pages/feed().php
@@ -3,7 +3,7 @@ if (!isset($_SESSION['settings'])) {
     $_SESSION['settings'] = DB::selectPairs('select `key`,`value` from settings');
 }
 $data = DB::select('select * from posts,users where posts.user_id = users.id and published is not null and published < NOW() order by published desc limit 10');
-DB::insert('insert into `unique_other_views` (`type`,`ip`,`day`,`requests`) values (3,?,DATE(NOW()),1) ON DUPLICATE KEY UPDATE `requests`=`requests`+1;',$_SERVER['REMOTE_ADDR']);
+DB::insert('insert into `unique_other_views` (`type`,`ip`,`day`,`requests`) values (?,?,DATE(NOW()),1) ON DUPLICATE KEY UPDATE `requests`=`requests`+1;','feed',$_SERVER['REMOTE_ADDR']);
 foreach (array_keys($data) as $i) {
   if ($pos=strpos($data[$i]['posts']['content'],'(...)')) {
     $data[$i]['posts']['intro'] = substr($data[$i]['posts']['content'], 0, $pos).'...';
--- pages/sitemap.xml().php
+++ pages/sitemap.xml().php
@@ -1,3 +1,3 @@
 <?php
 $data = DB::select('select slug, modified from posts where published is not null and published < NOW() order by published');
-DB::insert('insert into `unique_other_views` (`type`,`ip`,`day`,`requests`) values (4,?,DATE(NOW()),1) ON DUPLICATE KEY UPDATE `requests`=`requests`+1;',$_SERVER['REMOTE_ADDR']);
+DB::insert('insert into `unique_other_views` (`type`,`ip`,`day`,`requests`) values (?,?,DATE(NOW()),1) ON DUPLICATE KEY UPDATE `requests`=`requests`+1;','sitemap',$_SERVER['REMOTE_ADDR']);

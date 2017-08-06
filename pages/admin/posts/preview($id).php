563e5733aff4573916dd079e7b77c83caf6ea2d4
Maurits van der Schee <maurits@vdschee.nl>
2017-08-06 13:51:55 +0200

    remove unused code

--- pages/admin/posts/preview($id).php
+++ pages/admin/posts/preview($id).php
@@ -5,15 +5,12 @@ if ($pos=strpos($data['posts']['content'],'(...)')) {
     $data['posts']['html'] = substr($data['posts']['content'], 0, $pos).'...';
 	$data['posts']['more_html'] = true;
 	$data['posts']['content'] = substr_replace($data['posts']['content'], '', $pos, 5);
-	$data['posts']['description'] = substr($data['posts']['content'],0,$pos).'...';
 } else if ($pos = strpos($data['posts']['content'],' ',200)) {
 	$data['posts']['html'] = substr($data['posts']['content'], 0, $pos).'...';
 	$data['posts']['more_html'] = true;
-	$data['posts']['description'] = substr($data['posts']['content'],0,$pos).'...';
 } else {
 	$data['posts']['html'] = $data['posts']['content'];
 	$data['posts']['more_html'] = false;
-	$data['posts']['description'] = $data['posts']['content'];
 }
 $title = $data['posts']['title'];
 

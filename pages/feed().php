46f034a11b6c026de8b4ea9d43de43be99d61aba
Maurits van der Schee <maurits@vdschee.nl>
2017-08-07 14:48:51 +0200

    bugfix feed

--- pages/feed().php
+++ pages/feed().php
@@ -1,12 +1,20 @@
 <?php
 $data = DB::select('select * from posts,users where posts.user_id = users.id and published is not null and published < NOW() order by published desc limit 10');
-DB::insert('insert into `unique_other_views` (`type`,`ip`,`day`,`referrer_id`,`user_agent_id`,`requests`) values (?,?,DATE(NOW()),?,?,1) ON DUPLICATE KEY UPDATE `requests`=`requests`+1;','feed',$_SERVER['REMOTE_ADDR'],$_SESSION['referrer_id'],$_SESSION['user_agent_id']);
+if (!isset($_SESSION['user'])) {
+	DB::insert('insert into `unique_other_views` (`type`,`ip`,`day`,`referrer_id`,`user_agent_id`,`requests`) values (?,?,DATE(NOW()),?,?,1) ON DUPLICATE KEY UPDATE `requests`=`requests`+1;','feed',$_SERVER['REMOTE_ADDR'],$_SESSION['referrer_id'],$_SESSION['user_agent_id']);
+}
 foreach (array_keys($data) as $i) {
   if ($pos=strpos($data[$i]['posts']['content'],'(...)')) {
     $data[$i]['posts']['intro'] = substr($data[$i]['posts']['content'], 0, $pos).'...';
-  	$data[$i]['posts']['content'] = substr_replace($data[$i]['posts']['content'], '', $pos, 5);
+    $data[$i]['posts']['content'] = substr_replace($data[$i]['posts']['content'], '', $pos, 5);
+  } else if ($pos = strpos($data[$i]['posts']['content'],' ',200)) {
+    $data[$i]['posts']['intro'] = substr($data[$i]['posts']['content'], 0, $pos).'...';
+    $data[$i]['posts']['content'] = $data[$i]['posts']['content'];
+  } else {
+    $data[$i]['posts']['intro'] = $data[$i]['posts']['content'];
+    $data[$i]['posts']['content'] = $data[$i]['posts']['content'];
   }
   Buffer::set("intro[$i]",trim(Michelf\Markdown::defaultTransform($data[$i]['posts']['intro'])));
-	Buffer::set("content[$i]",trim(Michelf\Markdown::defaultTransform($data[$i]['posts']['content'])));
+  Buffer::set("content[$i]",trim(Michelf\Markdown::defaultTransform($data[$i]['posts']['content'])));
 }
 Debugger::$enabled=false;

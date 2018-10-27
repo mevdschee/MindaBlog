dedd37989efabc862808c814d284ba23b7204a53
Maurits van der Schee <maurits@vdschee.nl>
2018-10-27 11:01:50 +0200

    rename

--- start.sh
+++ start.sh
@@ -3,10 +3,10 @@ if [ ! -f composer.phar ]; then
     wget https://getcomposer.org/composer.phar
 fi
 php composer.phar update
-php vendor/MintyPHP/tools/requirements.php
+php vendor/mintyphp/tools/requirements.php
 if [[ $? != 0 ]]; then
     exit
 fi
-cp -r vendor/MintyPHP/debugger web/
-php -t web/ -S localhost:8000 vendor/MintyPHP/tools/server.php
+cp -r vendor/mintyphp/debugger web/
+php -t web/ -S localhost:8000 vendor/mintyphp/tools/server.php
 rm -Rf web/debugger

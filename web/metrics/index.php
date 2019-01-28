71e7723d4316ef099b98027ab5fce490aefbb0d2
Maurits van der Schee <maurits@vdschee.nl>
2019-01-28 23:52:36 +0100

    add metrics

---
+++ web/metrics/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en"><head>
+    <meta charset="utf-8">
+  
+    <meta name="viewport" content="width=device-width">
+  
+  <title>mevdschee/tqdev-metrics: Light-weight Java library to measure the behavior of critical components in a production environment.</title>
+
+  <meta name="description" content="tqdev-metrics - Light-weight Java library to measure the behavior of critical components in a production environment.">
+
+<style>
+.single-column { max-width: 50em; margin: 0 auto;}
+</style>
+  
+  </head>
+
+  <body>
+    <div class="single-column">
+      <article>
+
+<?php
+include 'MarkDown.php';
+use Michelf\Markdown;
+echo Markdown::defaultTransform(file_get_contents("https://raw.githubusercontent.com/mevdschee/tqdev-metrics/master/README.md"));
+?>
+
+<a href="https://github.com/mevdschee/tqdev-metrics">
+  <img style="position: absolute; top: 0; right: 0; border: 0;" src="forkme_right_darkblue_121621.png" alt="Fork me on GitHub">
+</a>
+</article>
+</div>
+</body></html>

7eb80eec36d16b58220ab9465420916d91025dac
Maurits van der Schee <maurits@vdschee.nl>
2019-05-01 00:00:55 +0200

    Add social share links

--- pages/index($slug).php
+++ pages/index($slug).php
@@ -1,17 +1,22 @@
 <?php
-$data = DB::selectOne('select * from posts,users where posts.user_id = users.id and slug = ? and published is not null and published < NOW();',$slug);
-if (!$data) Router::redirect('');
-if (!isset($_SESSION['user'])) {
-	DB::insert('insert into `unique_views` (`post_id`,`ip`,`day`,`referrer_id`,`user_agent_id`,`requests`) values (?,?,DATE(NOW()),?,?,1) ON DUPLICATE KEY UPDATE `requests`=`requests`+1;',$data['posts']['id'],$_SERVER['REMOTE_ADDR'],$_SESSION['referrer_id'],$_SESSION['user_agent_id']);
+$data = DB::selectOne('select * from posts,users where posts.user_id = users.id and slug = ? and published is not null and published < NOW();', $slug);
+if (!$data) {
+    Router::redirect('');
 }
-if ($pos=strpos($data['posts']['content'],'(...)')) {
-	$data['posts']['content'] = substr_replace($data['posts']['content'], '', $pos, 5);
-	$data['posts']['description'] = substr($data['posts']['content'],0,$pos).'...';
-} else if ($pos = strpos($data['posts']['content'],' ',200)) {
-	$data['posts']['description'] = substr($data['posts']['content'],0,$pos).'...';
+
+if (!isset($_SESSION['user'])) {
+    DB::insert('insert into `unique_views` (`post_id`,`ip`,`day`,`referrer_id`,`user_agent_id`,`requests`) values (?,?,DATE(NOW()),?,?,1) ON DUPLICATE KEY UPDATE `requests`=`requests`+1;', $data['posts']['id'], $_SERVER['REMOTE_ADDR'], $_SESSION['referrer_id'], $_SESSION['user_agent_id']);
+}
+if ($pos = strpos($data['posts']['content'], '(...)')) {
+    $data['posts']['content'] = substr_replace($data['posts']['content'], '', $pos, 5);
+    $data['posts']['description'] = substr($data['posts']['content'], 0, $pos) . '...';
+} else if ($pos = strpos($data['posts']['content'], ' ', 200)) {
+    $data['posts']['description'] = substr($data['posts']['content'], 0, $pos) . '...';
 } else {
-	$data['posts']['description'] = $data['posts']['content'];
+    $data['posts']['description'] = $data['posts']['content'];
 }
 $data['posts']['description'] = trim(strip_tags(Michelf\Markdown::defaultTransform($data['posts']['description'])));
 $title = $data['posts']['title'];
-Buffer::set('content',Michelf\Markdown::defaultTransform($data['posts']['content']));
+Buffer::set('content', Michelf\Markdown::defaultTransform($data['posts']['content']));
+$url = rtrim(Router::getBaseUrl(), '/') . Router::getRequest();
+$status = "$title... see: $url";

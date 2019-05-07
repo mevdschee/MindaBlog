692c4b5c8c3592bc660e5094647114528866ecb6
Maurits van der Schee <maurits@vdschee.nl>
2019-05-07 23:32:35 +0200

    improve graphs

--- lib/Graph.php
+++ lib/Graph.php
@@ -3,91 +3,112 @@ namespace MintyPHP;
 
 class Graph
 {
-	public static function verticalBar($values,$height,$title='',$description='') {
-		$real_max = max($values);
-		$max = pow(10,ceil(log10($real_max)));
-		while ($max/2>$real_max) $max/=2;
-		$html = '<div>';
-		$html.= '<div style="position: relative; clear: both; text-align: center;">';
-		$html.= $title.'</div>';
-		for ($i=0;$i<10;$i++) {
-			if ($i%2==0) {
-				$html.= '<div style="position: relative; top: '.($i/10*$height).'px; width: 100%;">';
-				$html.= '<div style="position: absolute; width: 100%; text-align: left; border-top: 1px solid #aaa;">';
-				$html.= '&nbsp;'.((1-$i/10)*$max);
-				$html.= '</div>';
-				$html.= '<div style="position: absolute; width: 100%; text-align: right; border-top: 1px solid #aaa;">';
-				$html.= ((1-$i/10)*$max).'&nbsp;';
-				$html.= '</div>';
-				$html.= '</div>';
-			} else {
-				$html.= '<div style="position: relative; top: '.($i/10*$height).'px; width: 100%;">';
-				$html.= '<div style="position: absolute; width: 100%; text-align: left; border-top: 1px solid #ccc;">';
-				$html.= '</div>';
-				$html.= '</div>';
-			}
-		}
-		$c = count($values);
-		foreach ($values as $key=>$value) {
-				$p = round(100*($value/$max));
-				$title = is_string($key)?$key.': '.$value:$value;
-				$html.= '<div style="float: right; width: '.(100/$c).'%; height: '.$height.'px;">';
-				$html.= '<div style="width: 100%; height: 100%; background-color: #eee;">';
-				$html.= '<a style="display: block; position: relative; margin: 0 10%; background-color: #aaa; height: '.$p.'%; top: '.(100-$p).'%" title="'.$title.'">';
-				$html.= '</a>';
-				$html.= '</div>';
-				$html.= '</div>';
-		}
-		$html.= '<div style="position: relative; clear:both; border-top: 1px solid #aaa;">';
-		$html.= $description.'</div>';
-		$html.= '</div>';
-		return $html;
-	}
-	
-	public static function horizontalBar($values,$height,$title='') {
-		$real_max = max($values);
-		$max = pow(10,ceil(log10($real_max)));
-		while ($max/2>$real_max) $max/=2;
-		$html = '<div>';
-		$html.= '<div style="position: relative; clear: both; text-align: center;">';
-		$html.= $title.'</div>';
-		$html.= '<div style="position: relative; height: '.$height.'px; background-color: #eee;">';
-		for ($i=0;$i<10;$i++) {
-			if ($i%2==0) {
-				$html.= '<div style="position: absolute; left: '.($i*10).'%; height: 100%; text-align: left; border-left: 1px solid #aaa;">';
-				$html.= '</div>';
-				$html.= '<div style="position: absolute; left: '.($i*10).'%; top: 100%;">';
-				$html.= '&nbsp;'.(($i/10)*$max);
-				$html.= '</div>';
-			} else {
-				$extra = $i==9?' border-right: 1px solid #aaa; width: 10%;':'';
-				$html.= '<div style="position: absolute; left: '.($i*10).'%; height: 100%; text-align: left; border-left: 1px solid #ccc;'.$extra.'">';
-				$html.= '</div>';
-			}
-		}
-		$html.= '</div>';
-		$c = count($values);
-		$row = ($height/$c);
-		$margin = .1*$row;
-		$line = $row-2*$margin;
-		$html.= '<div style="position: relative; margin-top: -'.$height.'px; height: '.$height.'px;">';
-		foreach ($values as $key=>$value) {
-				$p = round(100*($value/$max));
-				$title = is_string($key)?$key.': '.$value:$value;
-				$html.= '<div style="position: relative; height: '.$row.'px">';
-				$html.= '<div style="width: 100%; height: 100%; padding:'.$margin.'px 0">';
-				$html.= '<a style="display: block; float: left; position: relative; background-color: #aaa; width: '.$p.'%; height: 100%;" title="'.$title.'">';
-				$html.= '</a>';
-				$html.= '<div style="float: left; position: relative; width: '.(100-$p).'%; height: 100%; line-height: '.$line.'px; overflow: hidden;">';
-				$html.= '&nbsp;'.(is_string($key)?$key:'').'</div>';
-				$html.= '</div>';
-				$html.= '</div>';
-		}
-		$html.= '</div>';
-		$html.= '<div style="position: relative;">';
-		$html.= '&nbsp;</div>';
-		$html.= '</div>';
-		return $html;
-	}
+    public static function verticalBar($values, $height, $title = '')
+    {
+        if (count($values)) {
+            $real_max = max($values);
+        } else {
+            $real_max = 100;
+        }
+        $max = pow(10, ceil(log10($real_max)));
+        while ($max / 2 > $real_max) {
+            $max /= 2;
+        }
+        $html = '<svg xmlns="http://www.w3.org/2000/svg" version="1.1" x="0" y="0" width="100%" height="' . $height . '">';
+        $html .= '<rect width="100%" height="100%" fill="#eee" style="stroke:#aaa;stroke-width:2px;"/>';
+        for ($i = 0; $i < 10; $i++) {
+            $top = ($i / 10 * $height);
+            if ($i % 2 == 0) {
+                $html .= '<line x1="0" y1="' . $top . '" x2="100%" y2="' . $top . '" style="stroke:#aaa;" />';
+            } else {
+                $html .= '<line x1="0" y1="' . $top . '" x2="100%" y2="' . $top . '" style="stroke:#ccc;" />';
+            }
+        }
+        $c = count($values);
+        if ($c) {
+            $i = 0;
+            foreach ($values as $key => $value) {
+                $p = round(100 * ($value / $max));
+                $hover = is_string($key) ? $key . ': ' . $value : $value;
+                $html .= '<g>';
+                $margin = 0.25;
+                $html .= '<rect x="' . (($c - $i - 1) * (100 / $c) + $margin) . '%" y="' . (100 - $p) . '%" width="' . (100 / $c - 2 * $margin) . '%" height="' . $p . '%" style="fill:#bbb"/>';
+                if (is_string($key)) {
+                    $html .= '<text x="' . (($c - $i - .5) * (100 / $c)) . '%" y="' . (100 - $p) . '%" style="writing-mode: sideways-lr;font-size: 75%;dominant-baseline:middle;text-anchor:start;fill:#eee;" transform="translate(1,4)">' . $key . '</text>';
+                }
+                $html .= '<title>' . $hover . '</title>';
+                $html .= '</g>';
+                $i++;
+            }
+        }
+        $html .= '<text x="50%" y="0" style="dominant-baseline:hanging;text-anchor:middle" transform="translate(0,2)">' . $title . '</text>';
+        for ($i = 0; $i < 10; $i++) {
+            $top = ($i / 10 * $height);
+            $text = ((1 - $i / 10) * $max);
+            if ($i % 2 == 0) {
+                $html .= '<line x1="0" y1="' . $top . '" x2="9" y2="' . $top . '" style="stroke:#000;" />';
+                $html .= '<text x="0" y="' . $top . '" style="dominant-baseline:hanging;text-anchor:start" transform="translate(2,2)">' . $text . '</text>';
+            } else {
+                $html .= '<line x1="0" y1="' . $top . '" x2="6" y2="' . $top . '" style="stroke:#000;" />';
+            }
+        }
+        $html .= '</svg>';
+        return $html;
+    }
+
+    public static function horizontalBar($values, $height, $title = '')
+    {
+        if (count($values)) {
+            $real_max = max($values);
+        } else {
+            $real_max = 100;
+        }
+        $max = pow(10, ceil(log10($real_max)));
+        while ($max / 2 > $real_max) {
+            $max /= 2;
+        }
+        array_unshift($values, 0);
+
+        $html = '<svg xmlns="http://www.w3.org/2000/svg" version="1.1" x="0" y="0" width="100%" height="' . $height . '">';
+        $html .= '<rect width="100%" height="100%" fill="#eee" style="stroke:#aaa;stroke-width:2px;"/>';
+        for ($i = 0; $i < 10; $i++) {
+            $left = $i * 10;
+            if ($i % 2 == 0) {
+                $html .= '<line x1="' . $left . '%" y1="0" x2="' . $left . '%" y2="' . $height . '" style="stroke:#aaa;" />';
+            } else {
+                $html .= '<line x1="' . $left . '%" y1="0" x2="' . $left . '%" y2="' . $height . '" style="stroke:#ccc;" />';
+            }
+        }
+        $c = count($values);
+        if ($c) {
+            $i = 0;
+            foreach ($values as $key => $value) {
+                $p = round(100 * ($value / $max));
+                $hover = is_string($key) ? $key . ': ' . $value : $value;
+                $spacing = 0.05;
+                $html .= '<g>';
+                $html .= '<rect x="0" y="' . (($i + $spacing) * ($height / $c)) . '" width="' . $p . '%" height="' . ((1 - 2 * $spacing) * ($height / $c)) . '" style="fill:#bbb" />';
+                if (is_string($key)) {
+                    $html .= '<text x="' . $p . '%" y="' . (($i + 0.5) * ($height / $c)) . '" style="font-size:90%;dominant-baseline:middle;text-anchor:end;fill:#eee;" transform="translate(-4,0)">' . $key . '</text>';
+                }
+                $html .= '<title>' . $hover . '</title>';
+                $html .= '</g>';
+                $i++;
+            }
+        }
+        $html .= '<text x="50%" y="0" style="dominant-baseline:hanging;text-anchor:middle" transform="translate(0,2)">' . $title . '</text>';
+        for ($i = 0; $i < 10; $i++) {
+            $left = $i * 10;
+            $text = ($i / 10) * $max;
+            if ($i % 2 == 0) {
+                $html .= '<line x1="' . $left . '%" y1="' . ($height - 9) . '" x2="' . $left . '%" y2="' . $height . '" style="stroke:#000;" />';
+                $html .= '<text x="' . $left . '%" y="' . $height . '" style="text-anchor:start" transform="translate(3,-3)">' . $text . '</text>';
+            } else {
+                $html .= '<line x1="' . $left . '%" y1="' . ($height - 6) . '" x2="' . $left . '%" y2="' . $height . '" style="stroke:#000;" />';
+            }
+        }
+        $html .= '</svg>';
+        return $html;
+    }
 
 }
